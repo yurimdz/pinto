94f08cc02d6319d9418d6e64bed11740d5137c87
Dann233 <ds005110@gmail.com>
2024-04-26 23:21:13 -0600

    Hola

---
+++ Contactos.java
@@ -0,0 +1,42 @@
+public class Contactos {
+
+    String nombre;
+    String numeroContacto;
+
+    
+    public Contactos() {
+    }
+
+
+    public Contactos(String nombre, String numeroContacto) {
+        this.nombre = nombre;
+        this.numeroContacto = numeroContacto;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getNumeroContacto() {
+        return numeroContacto;
+    }
+
+
+    public void setNumeroContacto(String numeroContacto) {
+        this.numeroContacto = numeroContacto;
+    }
+
+
+
+
+
+
+
+}
