283cd6ba3fb3ec49f293cb5d45511ddcdf7a05ec
Dann233 <ds005110@gmail.com>
2024-04-27 00:19:40 -0600

    agregue tod0

--- Main.java
+++ Main.java
@@ -1,15 +1,66 @@
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 
 public class Main {
 
- String Opcion;
+    public static int Opcion;
+
+    public static String temporal;
 
     public static void main(String[] args) {
+        ArrayList<Contactos> Registro = new ArrayList<Contactos>();
+       
 
         JOptionPane.showMessageDialog(null,"Bienvenido a la agenda de contactos" );
-
-        JOptionPane.showInputDialog(null,"Digite una opción \n1. Añadir contacto \n2.Buscar contacto \n3. Eliminar contacto \n4. Salir");
+        menu ();
+       
         
+
+    }
+
+
+    
+    public static void menu (){
+        for(;;){
+        temporal = JOptionPane.showInputDialog(null,"Digite una opción \n1. Añadir contacto \n2.Buscar contacto \n3. Eliminar contacto \n4. Salir");
+        Opcion = Integer.parseInt(temporal);
+        
+        
+        switch (Opcion){
+            
+            case 1:
+            agendar ();
+
+
+            break;
+
+            case 2:
+            JOptionPane.showMessageDialog(null,"2" );
+            break;
+
+
+            case 3:
+            JOptionPane.showMessageDialog(null,"3" );
+            break;
+
+            case 4:
+            JOptionPane.showMessageDialog(null,"4" );
+            break;
+
+            default:
+            JOptionPane.showMessageDialog(null,"marque bien" );
+            break;
+        }
+
+    }
+
+    }
+
+    public static void agendar (){
+
+        
+
 
     }
 
