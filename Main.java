371626461bd551d755e6a085dce34128746fbf13
yurimdz <YURIDIA.MENDOZA@ucr.ac.cr>
2024-04-26 23:30:57 -0600

    creación de opciones

--- Main.java
+++ Main.java
@@ -2,8 +2,14 @@ import javax.swing.JOptionPane;
 
 public class Main {
 
+ String Opcion;
+
     public static void main(String[] args) {
+
         JOptionPane.showMessageDialog(null,"Bienvenido a la agenda de contactos" );
+
+        JOptionPane.showInputDialog(null,"Digite una opción \n1. Añadir contacto \n2.Buscar contacto \n3. Eliminar contacto \n4. Salir");
+        
 
     }
 
