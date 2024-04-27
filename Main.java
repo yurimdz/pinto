f95e5d27149a6f9bf1965e9f5402cfcb3d3cddc7
yurimdz <YURIDIA.MENDOZA@ucr.ac.cr>
2024-04-27 00:49:47 -0600

    arraylist

--- Main.java
+++ Main.java
@@ -9,18 +9,13 @@ public class Main {
     public static String temporal;
 
     public static void main(String[] args) {
+
         ArrayList<Contactos> Registro = new ArrayList<Contactos>();
        
-
         JOptionPane.showMessageDialog(null,"Bienvenido a la agenda de contactos" );
         menu ();
-       
-        
-
     }
 
-
-    
     public static void menu (){
         for(;;){
         temporal = JOptionPane.showInputDialog(null,"Digite una opción \n1. Añadir contacto \n2.Buscar contacto \n3. Eliminar contacto \n4. Salir");
@@ -59,9 +54,9 @@ public class Main {
 
     public static void agendar (){
 
-        
-
-
-    }
+        JOptionPane.showInputDialog(null,"Ingrese el nombre del contacto: " );
+        JOptionPane.showInputDialog(null,"Ingrese el número del contacto: " );
+            
+}
 
 }
