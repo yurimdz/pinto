6e709860a81a5861e9bbc42b014a0e0db95fa05c
Dann233 <ds005110@gmail.com>
2024-04-26 22:54:56 -0600

    Creacion de clase constructora

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Bienvenido a la agenda de contactos" );
+
+    }
+
+}
